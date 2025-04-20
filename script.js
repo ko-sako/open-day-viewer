88b9c511f8cedf8909a0fcaa3ab81690428f4512
Kayo Osako <osakok@cardiff.ac.uk>
2025-04-20 23:49:57 +0100

    Display event description and coverimage

--- script.js
+++ script.js
@@ -2,6 +2,26 @@ fetch('OpenDay.json')
     .then(response => response.json())
     .then(data => {
         // Event information
+        const header = document.getElementById('openDayHeader');
+
+        const eventCoverImageContainer = document.createElement('div');
+        eventCoverImageContainer.className = 'event-cover-container';
+        const eventCoverImage = document.createElement('img');
+        eventCoverImage.src = data.cover_image;
+        eventCoverImage.alt = 'Open Day Cover Image';
+        eventCoverImage.className = 'event-cover';
+
+        const eventDiscription = document.createElement('h2');
+        eventDiscription.textContent = data.description;
+        eventDiscription.className = 'event-description';
+
+        header.appendChild(eventDiscription);
+        header.appendChild(eventCoverImageContainer);
+        eventCoverImageContainer.appendChild(eventCoverImage);
+
+
+
+        // School Information
         const topics = data.topics;
         const container = document.getElementById('topicsContainer');
 
